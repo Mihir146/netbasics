57df03f62e7bf432fc5b1d5700f925db1375453e
Mihir146 <mihirv416@gmail.com>
2025-03-05 23:06:24 +0530

    make server run untill ctrl-c

--- webserver.py
+++ webserver.py
@@ -10,20 +10,21 @@ def srvr(portnum):
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(('', portnum))  # can maybe change '' to '127.0.0.1'??
     s.listen()
-    fd, addr = s.accept()
-    new_sock = fd
-    request = b""
     while 1:
-        # request += new_sock.recv(8)
-        request = new_sock.recv(300).decode("ISO-8859-1")
+        fd, addr = s.accept()
+        new_sock = fd
+        request = b""
+        while 1:
+            # request += new_sock.recv(8)
+            request = new_sock.recv(300).decode("ISO-8859-1")
 
 #            if (request & ((1 << 33)-1).to_bytes(4096, 'big') == b"\r\n\r\n"):
 #                break
-        if (request.find("\r\n\r\n") != -1):
-            break
-    response = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 6\r\n\r\nHello!"
-    new_sock.sendall(response)
-    new_sock.close()
+            if (request.find("\r\n\r\n") != -1):
+                break
+        response = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 6\r\n\r\nHello!"
+        new_sock.sendall(response)
+        new_sock.close()
 
 
 portnum = int(sys.argv[1])
